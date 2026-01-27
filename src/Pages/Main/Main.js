5bdc98cdf2361a8fa8df5d35d4122f1918c01191
Nick <64158017+Nick0070@users.noreply.github.com>
2026-01-27 10:28:30 +0300

    title

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -8,7 +8,6 @@ import { useNavigate } from 'react-router-dom';
 export const Main = () => {
     const navigate = useNavigate();
 
-
     // Прокрутка к "Записаться"
     const scrollToAppointment = () => {
       const element = document.getElementById('appointment-section');
@@ -18,44 +17,43 @@ export const Main = () => {
 
     return (
 
+       <div>
+
+            <div className='fon'>
+              <div className='container'>
+                <div className='prelude__container'>
+                  <div className='prelude__mass'>
+                    <div className='prelude_title'>
+                      <div className='lozyng'>
+                        Ремонт с комфортом – для вас и вашей машины  
+                      </div>
+                      <div className='loz'>
+                        С любовью АвтоХаус!
+                      </div>
+                      
+                      <div className='button_title_line'>
+                        <button className='button_title' onClick={scrollToAppointment}>О компании</button>
+                        <button className='button_title2' onClick={scrollToAppointment}>Каталог</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
 
 
 
-      <div>
-        <div className='fon'>
-          <div className='lozyng'>
-          Ремонт с комфортом – для вас и вашей машины  
-          </div>
 
-          <div className='loz'>
-            С любовью АвтоХаус!
-          </div>
-          <button className='btn2'  onClick={scrollToAppointment}> Записаться </button>
-         </div>
-        
-      <div className="main">      
-      {/* Блок Автозапчасти */}
-      <div className="box dark">
-        <div className="text">
-          <h2>Автозапчасти</h2>
-          <p>Подберем запчасть по лучшей цене</p>
-          <button className="button" onClick={() => navigate('/catalog')} >Подробнее</button>
-        </div>
-        <img className="image" src="image/zap.jpg" alt="Автозапчасти" />
+          
       </div>
-
-      {/* Блок Автосервис */}
-      <div className="box grey">
-        <div className="text">
-          <h2>Автосервис</h2>
-          <p>Ответственный ремонт авто для хороших людей</p>
-          <button className="button">Подробнее</button>
-        </div>
-        <img className="image" src="image/repair.jpg" alt="Автосервис" />
-      </div>
-    </div>
-    </div>
 
     );
 }
+
+
+
+
+
+
+
